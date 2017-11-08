455fa915924c4085b8a49ea37f11aa2cc10434e1
Vyacheslav Mukhin <slavadevelopers@gmail.com>
2017-11-08 21:58:55 +0300

    Test B4 is completed

---
+++ B4-smallerOfTheTwo/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a > b) {
+        cout << 2 << endl;
+    } else {
+        cout << 1 << endl;
+    }
+    return 0;
+}
